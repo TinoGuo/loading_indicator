efa22d6b6206129c1e332f1381176069a6d1efeb
TinoGuo <TinoGuo@users.noreply.github.com>
2022-01-22 08:13:17 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@f21bd537da0cf5b27951d9f2039a6566323a5d39 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,21 +3,25 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "4b6db237b3514a88107a422469adfb0f",
+  "index.html": "00fa4d0e5c5956ceb97755d9981ce3d0",
+"/": "00fa4d0e5c5956ceb97755d9981ce3d0",
+"version.json": "ff966ab969ba381b900e61629bfb9789",
 "manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
+"main.dart.js": "1c54ccb368bb82669e65ad9aac3e8e43",
+"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
+"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
+"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
+"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "066b49657ad45b4c4d4af46dd4ebf008",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "00fa4d0e5c5956ceb97755d9981ce3d0",
-"/": "00fa4d0e5c5956ceb97755d9981ce3d0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "f73e6a3551c9633cb7e373fcb03c06ca",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e"
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "a4be148b5e51c50791d7494158603890"
 };
 
 // The application shell files that are downloaded before a service worker can
