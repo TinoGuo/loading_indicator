e7efcceafadde1b1fc2d1a93c9f8fcd691c03400
TinoGuo <TinoGuo@users.noreply.github.com>
2023-06-08 08:10:51 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@6b893d8d32ffff0ed497406a4abc3a90c7ba6220 🚀

--- main.dart.js
+++ main.dart.js
@@ -20949,7 +20949,7 @@ $1(a){var s=$.cu
 s=(s==null?$.cu=A.fo(self.window.flutterConfiguration):s).b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/90fa3ae28fe6ddaee1af2c120f01e50201c1401b/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/2a3401c9bbb5a9a9aec74d4f735d18a9dd3ebf2d/":s)+a},
 $S:119}
 A.a67.prototype={
 $1(a){this.a.remove()
