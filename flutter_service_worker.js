1fd2058a58c9ca0b4b3bfa6aec2239dfb661eadd
TinoGuo <TinoGuo@users.noreply.github.com>
2025-04-08 08:09:13 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@271db9eb52b85daab1e632b5381647dc264f1454 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,7 +34,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "00e0b69b49487ce4f9ff0c5fac8fda49",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f9a0f1b9407c0c44f736efc387cbadb2"};
+"flutter_bootstrap.js": "84cd038c31b618acad7576084c9cba3b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
