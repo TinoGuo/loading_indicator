5067ac4738847d05621198563ed8686853e2d6ea
TinoGuo <TinoGuo@users.noreply.github.com>
2025-06-29 08:10:47 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@271db9eb52b85daab1e632b5381647dc264f1454 🚀

--- main.dart.js
+++ main.dart.js
@@ -20632,7 +20632,7 @@ return n}}
 A.a8p.prototype={
 $1(a){var s=A.dp().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/8cd19e509d6bece8ccd74aef027c4ca947363095/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/dd93de6fb1776398bf586cbd477deade1391c7e4/":s)+a},
 $S:107}
 A.P5.prototype={
 dd(a,b){var s,r=this.a
