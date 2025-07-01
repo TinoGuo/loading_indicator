d884fc4a5e89af93d5f83adc629d6ca292ad9a53
TinoGuo <TinoGuo@users.noreply.github.com>
2025-07-01 08:10:45 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@271db9eb52b85daab1e632b5381647dc264f1454 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "820556078"
+    serviceWorkerVersion: "3187585424"
   }
 });
