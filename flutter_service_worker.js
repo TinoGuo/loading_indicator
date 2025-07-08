348bfdc83aa7a12868d7e5aafd85ab6554fab21f
TinoGuo <TinoGuo@users.noreply.github.com>
2025-07-08 08:10:55 +0000

    Deploying to gh-pages from @ TinoGuo/loading_indicator@271db9eb52b85daab1e632b5381647dc264f1454 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"flutter_bootstrap.js": "caeaf88399006f8a1fc0653ea9c23223",
+"flutter_bootstrap.js": "669f096c795cb929027ca62aeb980ced",
 "version.json": "ff966ab969ba381b900e61629bfb9789"};
 // The application shell files that are downloaded before a service worker can
 // start.
